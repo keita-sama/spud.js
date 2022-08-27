733cb845aef6c1569cfd63a31dfadb25c28c8b4b
TECHNOPAWS <101276416+TECHNOPAWS@users.noreply.github.com>
2022-08-27 15:06:34 +0530

    Engleesh

--- src/BaseBuilder.js
+++ src/BaseBuilder.js
@@ -9,35 +9,35 @@ class BaseBuilder {
 
     setTime(duration) {
         if (!correctType('number', duration)) {
-            throw new SpudJSError(`Expected "number", got ${typeof duration}`);
+            throw new SpudJSError(`Expected "number", got ${typeof duration} instead.`);
         }
         this.time = duration;
         return this;
     }
     setFilter(filter) {
         if (!correctType('function', filter)) {
-            throw new SpudJSError(`Expected "function", got ${typeof filter}`);
+            throw new SpudJSError(`Expected "function", got ${typeof filter} instead`);
         }
         this.filter = filter;
         return this;
     }
     setMax(max) {
         if (!correctType('number', max)) {
-            throw new SpudJSError(`Expected "number", got ${typeof max}`);
+            throw new SpudJSError(`Expected "number", got ${typeof max} instead`);
         }
         this.max = max;
         return this;
     }
     setIdle(idle) {
         if (!correctType('boolean', idle)) {
-            throw new SpudJSError(`Expected "boolean", got ${typeof idle}`);
+            throw new SpudJSError(`Expected "boolean", got ${typeof idle} instead`);
         }
         this.idle = idle;
         return this;
     }
     disableMention(bool) {
         if (!correctType('boolean', bool)) {
-            throw new SpudJSError(`Expected "boolean", got ${typeof bool}`);
+            throw new SpudJSError(`Expected "boolean", got ${typeof bool} instead`);
         }
         this.shouldMention = !bool;
         return this;
