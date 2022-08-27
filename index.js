e98f5510e1be6e3f5600683c874956d07998245d
/Keita <87774086+keita-sama@users.noreply.github.com>
2022-08-27 23:00:28 +0200

    Added missing export

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.MenuBuilder = require('./src/MenuBuilder');
 exports.PaginationBuilder = require('./src/PaginationBuilder');
+exports.MenuOption = require('./src/options/MenuOption');
 exports.version = '3.0.0-dev';
