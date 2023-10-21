47a53c05e10b30d3bd91fb751038fb4b3093e720
Zeptar <thezeptar@gmail.com>
2023-10-22 07:04:24 +1300

    fix: Dumb mistake
    I accidentally put module.exports.[key] = exports.[key], way unsimpler than exports.[key]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-module.exports.MenuBuilder = exports.MenuBuilder = require('./builders/MenuBuilder');
-module.exports.PaginationBuilder = exports.PaginationBuilder = require('./builders/PaginationBuilder');
-module.exports.MenuOption = exports.MenuOption = require('./options/MenuOption');
-module.exports.GroupBuilder = exports.GroupBuilder = require('./builders/HybridBuilder');
-module.exports.version = exports.version = require('../package.json').version;
+exports.MenuBuilder = require('./builders/MenuBuilder');
+exports.PaginationBuilder = require('./builders/PaginationBuilder');
+exports.MenuOption = require('./options/MenuOption');
+exports.GroupBuilder = require('./builders/HybridBuilder');
+exports.version = require('../package.json').version;
